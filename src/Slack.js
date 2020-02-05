021b1afb738a289be21f186367c822fe1ff80f57
strangefish <strangefish2000@gmail.com>
2020-02-05 15:55:23 +0000

    Creating container, channel panel and people panel

--- src/Slack.js
+++ src/Slack.js
@@ -1,3 +1,21 @@
 import React from "react";
 
+const ChannelPanel = () => <span className="channel-panel">ChannelPanel</span>;
+
+const PeoplePanel = () => <span className="people-panel">PeoplePanel</span>;
+
+class Slack extends React.Component {
+  state = {};
+
+  render() {
+    return (
+      <div className="slack-container">
+        Slack container
+        <ChannelPanel />
+        <PeoplePanel />
+      </div>
+    );
+  }
+}
+
 export default Slack;
