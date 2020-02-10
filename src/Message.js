51c48741ae0bd1ff29939b171c4bea2a05e2b34a
strangefish <strangefish2000@gmail.com>
2020-02-10 17:11:33 +0000

    Message Component sketched out but not yet working. BACKUP POINT. I'm going to make changes - only ONE active component.

---
+++ src/Message.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./message.css";
+
+const Message = () => {
+  <div className="message-container">
+    <p className="message-text">A MESSAGE</p>
+  </div>;
+};
+
+export default Message;
