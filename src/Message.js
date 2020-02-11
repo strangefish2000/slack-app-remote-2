4b0f0af22b3a3576dbe86151dc36318632ee5c4a
strangefish <strangefish2000@gmail.com>
2020-02-11 15:28:23 +0000

    BACKUP POINT. fakeMessages saving to state. And can generate a list from them using .map

--- src/Message.js
+++ src/Message.js
@@ -1,17 +1,14 @@
 import React from "react";
-import "./message.css";
 
 class Message extends React.Component {
   render() {
     const { message } = this.props;
     return (
       <div className="message-container">
-        <p className="message-text">A MESSAGE</p>
+        <p className="message-text">{message.messageText}</p>
       </div>
     );
   }
 }
 
 export default Message;
-
-//...........
--- src/Messages.js
+++ src/Messages.js
@@ -1,27 +1,17 @@
 import React from "react";
 import "./messages.css";
-// import Message from "Message.js";
-
-// class Messages extends React.Component {
-//   render() {
-//     const { messages } = this.state;
-//     return (
-//       <ul className="messages">
-//         {messages.map(message => (
-//           <li key={message.id}>
-//             <Message message={message} />
-//           </li>
-//         ))}
-//       </ul>
-//     );
-//   }
-// }
+import Message from "./Message.js";
 
 class Messages extends React.Component {
   render() {
+    const { messages } = this.props;
     return (
       <ul className="messages">
-        <li>MESSAGE 1</li>
+        {messages.map(message => (
+          <li key={message.id}>
+            <Message message={message} />
+          </li>
+        ))}
       </ul>
     );
   }
